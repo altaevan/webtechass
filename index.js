0dafea1a7f25c4822467fcb7a26c32d37bebb067
altaevan <97723709+altaevan@users.noreply.github.com>
2022-02-22 08:33:21 +0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
-alert("hello");
+$("button").click(function (){
+    $("button").css("color", "brown");
+})
+
+$("h1").mouseover(function (){
+    $("h1").css("color", "brown");
+})
+
+$(".sound").click(function (){
+    var audio = new Audio('crash.mp3');
+    audio.play();
+    })
+
+
+
+
+
+
+
+
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+}
+
+
+
+
+
+
+
